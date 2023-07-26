1012050c10a8bbd95b6f9b9200902299be5416b6
Pratik Mukherjee <codingtricks.csc@gmail.com>
2023-07-26 16:33:41 +0530

    Update main.py

--- Software Engineering/Gantt Chart/main.py
+++ Software Engineering/Gantt Chart/main.py
@@ -15,8 +15,13 @@ df = pd.DataFrame(
     )
 )
 
-plt.barh(y=df['task'], width=df['duration'], left=df['time_to_start'])
+plt.barh(y=df['task'], width=df['duration'], left=df['time_to_start'], color='maroon')
 plt.title('Project Management Schedule', fontsize=15)
+plt.xlabel("<---- Time ---->")
+plt.ylabel("<---- Tasks ---->")
+plt.savefig("Graph_Outputs/graph.png")
 plt.show()
+
+
 
 
